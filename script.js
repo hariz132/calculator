ee1732abb5177b62bf6fce126be1ae3754a6975e
Hariz Halidi <hariz.halidi@gmail.com>
2022-01-24 22:26:50 +0800

    basic functionality inplemented

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
+const display = document.querySelector('.display')
+const buttons = document.querySelectorAll('.buttons button');
+const digitButtons = document.querySelectorAll('.buttons .digit');
+const operatorButtons = document.querySelectorAll('.buttons .operator');
+const equalButton = document.querySelector('#equal');
+let displayValue = '';
+let operator;
+let savedValue;
+
+for (const button of digitButtons) {
+  button.addEventListener('click', e => {
+    displayValue += e.target.textContent;
+    updateDisplay(displayValue);
+  });
+}
+
+for (const button of operatorButtons) {
+  button.addEventListener('click', e => {
+    if (savedValue) {
+      displayValue = operate(operator, +savedValue, +displayValue);
+      updateDisplay(displayValue);
+    }
+    operator = e.target.id;
+    savedValue = displayValue;
+    displayValue = '';
+  });
+}
+
+equalButton.addEventListener('click', e => {
+  if (savedValue) {
+    displayValue = operate(operator, +savedValue, +displayValue);
+    updateDisplay(displayValue);
+  }
+  savedValue = displayValue;
+  displayValue = '';
+});
+
+function updateDisplay(str) {
+  display.textContent = str;
+}
+
+function operate(operator, a, b) {
+  switch (operator) {
+    case 'add':
+      return add(a, b);
+    case 'subtract':
+      return subtract(a, b);
+    case 'multiply':
+      return multiply(a, b);
+    case 'divide':
+      return divide(a, b);
+  }
+}
+
 function add(a, b) {
   return a + b;
 }
@@ -12,17 +66,4 @@ function multiply(a, b) {
 
 function divide(a, b) {
   return a / b;
-}
-
-function operate(operator, a, b) {
-  switch (operator) {
-    case 'add':
-      return add(a, b);
-    case 'subtract':
-      return subtract(a, b);
-    case 'multiply':
-      return multiply(a, b);
-    case 'divide':
-      return divide(a, b);
-  }
 }
